b961f9e20b5ffe9d6db8ddbb05766d440b86ac04
Matthieu Lagacherie <matthieu@lagacherie.fr>
2014-12-02 22:03:24 +0000

    update ssh keys

--- gen-ssh.sh
+++ gen-ssh.sh
@@ -1,5 +1,4 @@
 ### SSH USER KEY FOR LOCAL SSH
-mkdir pom/.ssh
 ssh-keygen -t rsa -C "pom@pgxl" -f pom/.ssh/id_rsa -q -N ""
-cat pom/.ssh/id_rsa.pub >> pom/.ssh/authorized_keys
+cp pom/.ssh/id_rsa.pub pom/.ssh/authorized_keys
 
--- pgxl/entrypoint.sh
+++ pgxl/entrypoint.sh
@@ -5,6 +5,11 @@ if [ $# == 0 ]; then
 elif [ $1 == "supervisor" ]; then
 	sudo sysctl -p
 	sudo /usr/bin/supervisord;
+elif [ $1 == "key" ]; then
+	mkdir /pom/.ssh
+	ssh-keygen -t rsa -C "pom@pgxl" -f /pom/.ssh/id_rsa -q -N ""
+	cp /pom/.ssh/id_rsa.pub /pom/.ssh/authorized_keys
+	chmod 700 /pom/.ssh && chmod 600 /pom/.ssh/*
 elif [ $1 == "shell" ]; then
 	/bin/bash;
 elif [ $1 == "ip" ]; then
