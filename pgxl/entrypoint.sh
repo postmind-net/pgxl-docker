bc6865429e874d2843f896f38264e17292cfd4b8
Matthieu Lagacherie <matthieu@lagacherie.fr>
2014-12-05 20:37:06 +0000

    user chown

--- pgxl/entrypoint.sh
+++ pgxl/entrypoint.sh
@@ -6,6 +6,7 @@ elif [ $1 == "supervisor" ]; then
 	sudo sysctl -p
 	sudo /usr/bin/supervisord;
 elif [ $1 == "key" ]; then
+	sudo chown -R pom:pom /pom
 	mkdir /pom/.ssh
 	ssh-keygen -t rsa -C "pom@pgxl" -f /pom/.ssh/id_rsa -q -N ""
 	cp /pom/.ssh/id_rsa.pub /pom/.ssh/authorized_keys
