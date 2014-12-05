2fd5828927de1bf0a849a3b8255826d503aa4e51
Lagacherie Matthieu <mlagacherie@octo.com>
2014-12-05 20:51:32 +0100

    adding gtm proxy + coordinators

--- run.py
+++ run.py
@@ -23,13 +23,15 @@ def get_containers(dcl):
         cts.append({"name": name, "ip": ip})
     return cts
 
-def get_conf(dcl):
-    ctn = get_containers(dcl)
-    datanodes = ["PGXL%d" %i for i in range(len(ctn))]
-    datanodes_ip = [c["ip"] for c in ctn]
-    gtm_server = datanodes_ip[0]
+def get_conf(ips):
+    servers = ["PGXL%d" %i for i in range(len(ips))]
+    servers_ip = ips
+    datanodes = servers[1:]
+    datanodes_ip = servers_ip[1:]
+    nnodes = len(datanodes)
+    gtm_server = servers_ip[0]
     gtm_port = 20001
-    coord_server = datanodes_ip[0]
+    #coord_server = datanodes_ip[0]
     conf = StringIO()
     conf.write("""
 pgxcOwner=pom
@@ -46,18 +48,34 @@ gtmMasterDir=$HOME/pgxc/nodes/gtm
 gtmExtraConfig=none
 gtmMasterSpecificExtraConfig=none
 """.format(gtm_server, gtm_port))
+
+    conf.write("""
+gtmProxyDir=$HOME/pgxc/nodes/gtm_pxy
+gtmProxy=y
+gtmProxyNames=({0})
+gtmProxyServers=({1})
+gtmProxyPorts=({2})
+gtmProxyDirs=({3})
+gtmPxyExtraConfig=none
+gtmPxySpecificExtraConfig=({4})
+""".format(' '.join(["PROXY%d" %i for i in range(nnodes)]),
+           ' '.join(datanodes_ip),
+           ' '.join(["20001"] * nnodes),
+           ' '.join(["$gtmProxyDir"] * nnodes),
+           ' '.join(["none"] * nnodes)))
+
     conf.write("""
 coordMasterDir=$HOME/pgxc/nodes/coord
 coordSlaveDir=$HOME/pgxc/nodes/coord_slave
 coordArchLogDir=$HOME/pgxc/nodes/coord_archlog
-coordNames=(coord1)
-coordPorts=(5432)
-poolerPorts=(20010)
-coordPgHbaEntries=(all)
-coordMasterServers=({0})
-coordMasterDirs=($coordMasterDir)
-coordMaxWALsernder=0
-coordMaxWALSenders=($coordMaxWALsernder)
+coordNames=({0})
+coordPorts=({1})
+poolerPorts=({2})
+coordPgHbaEntries=({3})
+coordMasterServers=({4})
+coordMasterDirs=({5})
+coordMaxWALsender=5
+coordMaxWALSenders=({6})
 coordSlave=n
 coordExtraConfig=coordExtraConfig
 cat > $coordExtraConfig <<EOF
@@ -71,10 +89,17 @@ listen_addresses = '*'
 max_connections = 100
 log_filename = 'coordinator.log'
 EOF
-coordSpecificExtraConfig=(none)
+coordSpecificExtraConfig=({7})
 coordExtraPgHba=none
-coordSpecificExtraPgHba=(none)
-""".format(coord_server))
+coordSpecificExtraPgHba=({7})
+""".format(' '.join("COORD%d" %node for node in range(nnodes)),
+           ' '.join(["5432"] * nnodes),
+           ' '.join(["20010"] * nnodes),
+           ' '.join(["all"] * nnodes),
+           ' '.join(datanodes_ip),
+           ' '.join("$coordMasterDir/%d" %i for i in range(nnodes)),
+           ' '.join(["$coordMaxWALsender"] * nnodes),
+           ' '.join(["none"] * nnodes)))
     conf.write("""
 datanodeMasterDir=$HOME/pgxc/nodes/dn_master
 datanodeSlaveDir=$HOME/pgxc/nodes/dn_slave
@@ -84,28 +109,69 @@ datanodeNames=({1})
 datanodePorts=({2})
 datanodePoolerPorts=({3})
 datanodePgHbaEntries=(all)
-""".format(datanodes[0], ' '.join(datanodes), ' '.join(["21000"] * len(datanodes)), ' '.join(["21010"] * len(datanodes))))
+""".format(datanodes[0], ' '.join(datanodes),
+           ' '.join(["21000"] * nnodes), ' '.join(["21010"] * nnodes)))
     conf.write("""
 datanodeMasterServers=({0})
 datanodeMasterDirs=({1})
 datanodeMaxWalSender=5
 datanodeMaxWALSenders=({3})
 datanodeSlave=n
-datanodeExtraConfig=none
+datanodeExtraConfig=node.conf
 datanodeSpecificExtraConfig=({2})
 datanodeExtraPgHba=none
 datanodeSpecificExtraPgHba=({2})
 datanodeAdditionalSlaves=n
-""".format(' '.join(ip for ip in datanodes_ip), ' '.join("$datanodeMasterDir/%d" %i for i in range(len(datanodes))),
-           ' '.join(["none"] * len(datanodes)), ' '.join(["$datanodeMaxWalSender"] * len(datanodes))))
+""".format(' '.join(ip for ip in datanodes_ip), ' '.join("$datanodeMasterDir/%d" %i for i in range(nnodes)),
+           ' '.join(["none"] * nnodes),
+           ' '.join(["$datanodeMaxWalSender"] * nnodes)))
+    return conf
+
+def get_haproxy(ips):
+    conf = StringIO()
+    conf.write("""
+global
+    log         127.0.0.1 local2 debug
+
+    chroot      /var/lib/haproxy
+    pidfile     /var/run/haproxy.pid
+    maxconn     4000
+    user        haproxy
+    group       haproxy
+    daemon
+
+    # turn on stats unix socket
+    stats socket /var/lib/haproxy/stats
+
+defaults
+    timeout client 1m
+    timeout server 1m
+    timeout connect 10s
+
+listen stats 0.0.0.0:9000
+    mode http
+    stats uri /stats
+
+listen PGSQL 0.0.0.0:5432
+    mode tcp
+    option tcplog
+    balance static-rr
+""")
+
+    for i in range(1, len(ips)):
+        conf.write("""    server s%d %s:5432 check\n""" %(i, ips[i]))
     return conf
 
 if args.ip:
     print get_containers(dcl)
 
 if args.conf:
-    conf = get_conf(dcl)
-    with open(args.conf, "w+") as fp:
+    ctn = get_containers(dcl)
+    ips = [c["ip"] for c in ctn]
+    conf = get_conf(ips)
+    with open("%s/pgxc_ctl.conf" %args.conf, "w+") as fp:
         fp.write(conf.getvalue())
+    with open("%s/haproxy.cfg" %args.conf, "w+") as fp:
+        fp.write(get_haproxy(ips).getvalue())
     conf.close()
 
